242652fb03c220833123db837be91d05dbc73581
Roman Samoilov <rsamoilov@productengine.com>
2015-03-18 15:33:05 +0200

    Add logrotate config

--- lib/oysters/initd.rb
+++ lib/oysters/initd.rb
@@ -106,6 +106,19 @@ Oysters.with_configuration do
             sudo rm -f /etc/init.d/#{application}_kewatcher", pty: true
       end
     end
+
+    namespace :logrotate do
+      desc 'Install logrotate config'
+      task :install, roles: :app do
+        log_path = Pathname.new(shared_path).join('log', "#{rails_env}.log").to_s
+        tmp_file = "/tmp/#{application}_logs_#{rand}"
+        location = File.expand_path('../templates/logrotate_config.erb', __FILE__)
+        config   = ERB.new(File.read(location))
+
+        put config.result(binding), tmp_file
+        sudo "mv #{tmp_file} /etc/logrotate.d/#{application}_logs"
+      end
+    end
   end
 
 end
