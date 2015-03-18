fa3cd09656a8a275b2918fa3cde62efb5743fe50
Petr Balaban <pbalaban@productengine.com>
2015-03-18 18:06:08 +0200

    Update logrotate.status after install.

--- lib/oysters/initd/logrotate.rb
+++ lib/oysters/initd/logrotate.rb
@@ -7,13 +7,16 @@ Oysters.with_configuration do
     namespace :logrotate do
       desc 'Install logrotate config'
       task :install, roles: :app do
-        log_path = Pathname.new(shared_path).join('log', "#{rails_env}.log").to_s
-        tmp_file = "/tmp/#{application}_logs_#{rand}"
-        location = File.expand_path('../../templates/logrotate_config.erb', __FILE__)
-        config   = ERB.new(File.read(location))
+        conf_path = "/etc/logrotate.d/#{application}_logs"
+        log_path  = Pathname.new(shared_path).join('log', "#{rails_env}.log").to_s
+        tmp_file  = "/tmp/#{application}_logs_#{rand}"
+        location  = File.expand_path('../../templates/logrotate_config.erb', __FILE__)
+        config    = ERB.new(File.read(location))
 
         put config.result(binding), tmp_file
-        sudo "mv #{tmp_file} /etc/logrotate.d/#{application}_logs"
+        sudo "mv #{tmp_file} #{conf_path}"
+
+        sudo "logrotate #{conf_path}"
       end
     end
 
