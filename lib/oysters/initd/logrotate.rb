5ebdc1262bac9d9cfead33670cc08d52846aa179
Aleksey Baranov <abaranov@productengine.com>
2015-04-29 17:35:37 +0300

    OPSINT-1251 Added specified logs for oiv-ui application in logrotate.

--- lib/oysters/initd/logrotate.rb
+++ lib/oysters/initd/logrotate.rb
@@ -7,8 +7,17 @@ Oysters.with_configuration do
     namespace :logrotate do
       desc 'Install logrotate config'
       task :install, roles: :app do
+        environment = Rails.env
+        log_path = [ Pathname.new(shared_path).join('log', "#{environment}.log").to_s ]
+
+        if application == 'oiv-ui'
+          %W(#{environment}_delayed_job.log #{environment}_security_audit.log unicorn_#{environment}.stderr.log).each do |log|
+            log_path << Pathname.new(shared_path).join('log', log).to_s
+          end
+        end
+
         conf_path = "/etc/logrotate.d/#{application}_logs"
-        log_path  = Pathname.new(shared_path).join('log', '*.log').to_s
+        log_path  = log_path.join(' ')
         tmp_file  = "/tmp/#{application}_logs_#{rand}"
         location  = File.expand_path('../../templates/logrotate_config.erb', __FILE__)
         config    = ERB.new(File.read(location))
