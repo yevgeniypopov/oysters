17fe9640843ae5f0cc205364cdf8106038005ef0
Aleksey Baranov <abaranov@productengine.com>
2015-04-29 15:50:51 +0300

    OPSINT-1251 Added all logs in the specified folder for logrotate.

--- lib/oysters/initd/logrotate.rb
+++ lib/oysters/initd/logrotate.rb
@@ -8,7 +8,7 @@ Oysters.with_configuration do
       desc 'Install logrotate config'
       task :install, roles: :app do
         conf_path = "/etc/logrotate.d/#{application}_logs"
-        log_path  = Pathname.new(shared_path).join('log', "#{rails_env}.log").to_s
+        log_path  = Pathname.new(shared_path).join('log', '*.log').to_s
         tmp_file  = "/tmp/#{application}_logs_#{rand}"
         location  = File.expand_path('../../templates/logrotate_config.erb', __FILE__)
         config    = ERB.new(File.read(location))
